6f357e046f614dbf72953a4ccc18e55c00a48a88
Gladys Ramos <gladysxramos@gmail.com>
2024-07-31 22:18:43 -0700

    Initial Commit

---
+++ PracticeMap/PracticeMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeMapApp.swift
+//  PracticeMap
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
